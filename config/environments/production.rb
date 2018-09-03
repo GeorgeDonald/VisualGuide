892921c59b0616560cfbc680dbe8a1a1780de3af
GeorgeDonald <dongsy326@gmail.com>
2018-09-03 10:42:14 -0400

    add redis support in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://virtra.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://virtra.herokuapp.com', 'http://virtra.herokuapp.com']
 end
